041953f98a339d488ca8860f1b095238bb8f6bbc
Евгений Михайлюков <evgeni.mihaylyukov@techincity.ru>
2022-01-31 17:34:07 +0300

    /

---
+++ frontend/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <header class="header">
+      <div class="container">
+        <div class="header__inner">
+          <div class="header__logo">Gunshop</div>
+
+          <nav class="nav">
+            <ul>
+              <li>
+                <router-link :to="{name: 'About'}">О нас</router-link>
+              </li>
+              <li>
+                <router-link :to="{name: 'Catalog'}">Каталог</router-link>
+              </li>
+              <li>
+                <router-link :to="{name: 'Stores'}">Наши салоны</router-link>
+              </li>v
+              <li>
+                <router-link :to="{name: 'Shipping'}">Доставка</router-link>
+              </li>
+              <li>
+                <router-link :to="{name: 'Contacts'}">Контакты</router-link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ frontend/src/views/Product.vue
@@ -0,0 +1,17 @@
+<template>
+ <section>
+   <h1>Страница товара</h1>
+ </section>
+</template>
+
+<script>
+export default {
+  name: "Product"
+
+
+}
+</script>
+
+<style scoped>
+
+</style>
